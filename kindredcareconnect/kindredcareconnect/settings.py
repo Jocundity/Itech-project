58097490a40b6a9c2f159b190f3cf255f5012521
Aasim3171501 <3171501K@student.gla.ac.uk>
2026-03-19 21:41:36 +0530

    change

--- kindredcareconnect/kindredcareconnect/settings.py
+++ kindredcareconnect/kindredcareconnect/settings.py
@@ -115,25 +115,19 @@ USE_I18N = True
 USE_TZ = True
 
 
+import os
+
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/6.0/howto/static-files/
-
 STATIC_URL = '/static/'
-
-MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-MEDIA_ROOT = BASE_DIR / 'media'
-
-# This tells the @login_required decorator where to redirect (when not signed in)
-LOGIN_URL = 'kindred:signin'
-
-import os
-
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# Production storage for WhiteNoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Media files (User uploads)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
